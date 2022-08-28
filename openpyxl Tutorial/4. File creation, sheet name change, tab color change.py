8ccdca38215a532e82aa23a879f7d618a1cdf5d9
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-08-29 00:09:22 +0900

    Update 4. File creation, sheet name change, tab color change.py

--- openpyxl Tutorial/4. File creation, sheet name change, tab color change.py
+++ openpyxl Tutorial/4. File creation, sheet name change, tab color change.py
@@ -1,7 +1,7 @@
 from openpyxl import Workbook
 
 wb = Workbook()
-ws = wb.active
-ws.title = 'test_sheet'
-wb.save('sample.xlsx')
-wb.close()
+ws = wb.active  # 활성화 된 시트를 담을 변수
+ws.title = 'test_sheet_4'  # 시트의 이름 변경
+wb.save('sample_4.xlsx')  # 현 파일 위치에 만들어진 엑셀파일을 'sample_4.xlsx'라는 이름으로 저장
+wb.close()  # 워크북을 닫아준다.
